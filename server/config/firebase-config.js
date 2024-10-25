afc1f7ea851db112d319a52af2c291cec57771dd
Akazamarz <114688016+akazamarz@users.noreply.github.com>
2024-10-25 21:25:51 +0100

    Update firebase-config.js

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,11 +1,21 @@
-// server/config/firebase-config.js
-const admin = require('firebase-admin');
-const serviceAccount = require('./degen-hunter-76883-firebase-adminsdk-v84sm-01492158ae.json');
+// firebase-config.js
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js';
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.14.1/firebase-database.js';
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://degen-hunter-76883-default-rtdb.firebaseio.com/'
-});
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD3EnHiULjNCjZp_dNwQ8pVUp1V5LyZy54",
+    authDomain: "degen-hunter-76883.firebaseapp.com",
+    databaseURL: "https://degen-hunter-76883-default-rtdb.firebaseio.com",
+    projectId: "degen-hunter-76883",
+    storageBucket: "degen-hunter-76883.appspot.com",
+    messagingSenderId: "218677301832",
+    appId: "1:218677301832:web:0d7d66c23645341ca07e7d",
+    measurementId: "G-G25PFHYJWE"
+};
 
-const db = admin.database();
-module.exports = db;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { app, database };
