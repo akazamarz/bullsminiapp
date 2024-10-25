c51389b8400f01d17eaa09c450c86dd13b522be4
Akazamarz <114688016+akazamarz@users.noreply.github.com>
2024-10-25 16:15:05 +0100

    Update firebase-config.js

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,10 +1,10 @@
 // server/config/firebase-config.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./path-to-your-firebase-admin-sdk.json');
+const serviceAccount = require('./degen-hunter-76883-default-rtdb-export.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://your-project-id.firebaseio.com'
+    databaseURL: 'https://degen-hunter-76883-default-rtdb.firebaseio.com/'
 });
 
 const db = admin.database();
