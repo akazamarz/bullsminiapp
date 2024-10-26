9c6473daa28a609c59f52c8d1b1843d988a645fa
Akazamarz <114688016+akazamarz@users.noreply.github.com>
2024-10-26 20:07:13 +0100

    Update firebase-config.js

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,11 +1,17 @@
 const admin = require('firebase-admin');
 
-// Parse the private key JSON from the environment variable
-const serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY);
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT environment variable is not set.");
+}
+
+// Parse the service account JSON from the environment variable
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, '\n')
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://degen-hunter-76883-default-rtdb.firebaseio.com'
+  databaseURL: "https://degen-hunter-76883-default-rtdb.firebaseio.com",
 });
 
 const db = admin.database();
