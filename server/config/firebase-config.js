42bcfc3679cf845f2a4ce6be99f24e92cc0ece80
Akazamarz <114688016+akazamarz@users.noreply.github.com>
2024-10-26 17:57:58 +0100

    Update firebase-config.js

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,24 +1,13 @@
-import admin from 'firebase-admin';
+const admin = require('firebase-admin');
 
-// Construct the service account object using environment variables
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Replace escaped \n with actual newlines
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
-};
+// Parse the JSON string from the environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://degen-hunter-76883-default-rtdb.firebaseio.com'
 });
 
 const db = admin.database();
-export default db;
+module.exports = db;
